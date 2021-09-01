1a568cc9b512a566827c7cf366b8dcdfdfde6097
JosephSilva362 <josephsilva362@gmail.com>
2021-09-01 13:38:20 -0300

    Test API

--- src/main/java/joseph/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/joseph/digitalinnovation/personapi/controllers/PersonController.java
@@ -1,4 +1,15 @@
 package joseph.digitalinnovation.personapi.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
 public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API Test";
+    }
 }
