937571eacccd0ed1db828124e91edab0ff4e11a9
JosephSilva362 <josephsilva362@gmail.com>
2021-09-01 14:06:40 -0300

    3. Entities criation

--- src/main/java/joseph/digitalinnovation/personapi/enums/PhoneType.java
+++ src/main/java/joseph/digitalinnovation/personapi/enums/PhoneType.java
@@ -1,4 +1,14 @@
 package joseph.digitalinnovation.personapi.enums;
 
-public class PhoneType {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
 }
