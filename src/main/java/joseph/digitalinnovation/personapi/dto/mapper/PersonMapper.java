7f522b7f3c3252e05a777c438e9d34f27684da76
JosephSilva362 <josephsilva362@gmail.com>
2021-09-01 14:48:38 -0300

    5. DTO

--- src/main/java/joseph/digitalinnovation/personapi/dto/mapper/PersonMapper.java
+++ src/main/java/joseph/digitalinnovation/personapi/dto/mapper/PersonMapper.java
@@ -1,4 +1,16 @@
 package joseph.digitalinnovation.personapi.dto.mapper;
 
+import joseph.digitalinnovation.personapi.dto.request.PersonDTO;
+import joseph.digitalinnovation.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")
 public interface PersonMapper {
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO dto);
+
+    PersonDTO toDTO(Person dto);
 }
